03959535569c9d3c6bd50eefb7aeeedf3e27c6f8
root <root@bullseye-template.odiso.net>
2023-07-07 11:31:05 +0200

    enable oidc

--- lib/modules/modules.go
+++ lib/modules/modules.go
@@ -409,6 +409,7 @@ func (p *defaultModules) Features() Features {
 		DB:                 true,
 		App:                true,
 		Desktop:            true,
+		OIDC:              true,
 		AutomaticUpgrades:  p.automaticUpgrades,
 		JoinActiveSessions: true,
 		SupportType:        proto.SupportType_SUPPORT_TYPE_FREE,
