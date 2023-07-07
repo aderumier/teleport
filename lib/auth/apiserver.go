2402053a0fa92a52318baa55250c2d94c85b2e2f
root <root@bullseye-template.odiso.net>
2023-07-07 12:50:53 +0200

    add oidc connector

--- lib/auth/apiserver.go
+++ lib/auth/apiserver.go
@@ -178,6 +178,7 @@ func NewAPIServer(config *APIConfig) (http.Handler, error) {
 
 	// SSO validation handlers
 	srv.POST("/:version/github/requests/validate", srv.WithAuth(srv.validateGithubAuthCallback))
+	srv.POST("/:version/oidc/requests/validate", srv.WithAuth(srv.validateOIDCAuthCallback))
 
 	// Audit logs AKA events
 	srv.GET("/:version/events", srv.WithAuth(srv.searchEvents))
@@ -620,6 +621,40 @@ func (s *APIServer) rotateExternalCertAuthority(auth *ServerWithRoles, w http.Re
 		return nil, trace.Wrap(err)
 	}
 	return message("ok"), nil
+}
+
+func (s *APIServer) validateOIDCAuthCallback(auth *ServerWithRoles, w http.ResponseWriter, r *http.Request, p httprouter.Params, version string) (interface{}, error) {
+	var req *ValidateOIDCAuthCallbackReq
+	if err := httplib.ReadJSON(r, &req); err != nil {
+		return nil, trace.Wrap(err)
+	}
+	response, err := auth.ValidateOIDCAuthCallback(r.Context(), req.Query)
+	if err != nil {
+		return nil, trace.Wrap(err)
+	}
+	raw := OIDCAuthRawResponse{
+		Username: response.Username,
+		Identity: response.Identity,
+		Cert:     response.Cert,
+		TLSCert:  response.TLSCert,
+		Req:      response.Req,
+	}
+	if response.Session != nil {
+		rawSession, err := services.MarshalWebSession(response.Session, services.WithVersion(version))
+		if err != nil {
+			return nil, trace.Wrap(err)
+		}
+		raw.Session = rawSession
+	}
+	raw.HostSigners = make([]json.RawMessage, len(response.HostSigners))
+	for i, ca := range response.HostSigners {
+		data, err := services.MarshalCertAuthority(ca, services.WithVersion(version))
+		if err != nil {
+			return nil, trace.Wrap(err)
+		}
+		raw.HostSigners[i] = data
+	}
+	return &raw, nil
 }
 
 // validateGithubAuthCallbackReq is a request to validate Github OAuth2 callback
